5442b5bfb35b72cbb682e0488c7778a98231cbda
truong <quangtruong.tdr012@gmail.com>
2022-09-01 17:16:44 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        bluex: "#0F1B4C",
+        seax: "#E6F0FF",
+        blackx: '#4F5361',
+        "blackx-50": '#687690'
+      },
+      width: {
+        '90': "22.5rem",
+        '96': "24rem",
+      }
+    },
+    container: {
+      padding: '10rem',
+    },
+  },
+  plugins: [],
+}
